bb2103d345dd1521ee7030e18c9dd5d55c85f7ab
PavithraMp23 <pavithramoffcl23@gmail.com>
2025-06-14 18:56:11 +0000

    added states

---
+++ smarter-tasks/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import TaskApp from "./TaskApp";
+function App() {
+  return (
+   <div className='App'>
+<div className="App">
+      <TaskApp />
+    </div>
+   </div>
+  )
+}
+
+export default App
---
+++ smarter-tasks/src/TaskList.tsx
@@ -0,0 +1,27 @@
+//we will be rendering the tasklist using the component Task
+
+import React from "react";
+import Task from "./Task";
+import type { TaskItem } from "./types";
+interface Props{
+    tasks: TaskItem[]; //interface TaskItem contains title, description and dueDate 
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface State{
+    
+}
+
+
+class TaskList extends React.Component<Props,State>{
+render(){
+    return this.props.tasks.map((task,idx)=>(
+        <Task 
+        key ={idx}
+        title={task.title}
+        description= {task.description}
+        dueDate = {task.dueDate} />
+    ));
+}
+}
+export default TaskList
